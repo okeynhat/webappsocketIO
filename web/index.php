70a5b1b698608ffffd004809ee91a5818b8229fe
okeynhat <okeynhat@users.noreply.github.com>
2020-01-29 21:57:28 +0700

    Cập nhật

--- web/index.php
+++ web/index.php
@@ -1,3 +1,7 @@
 <?php
-echo "Hệ Thống Được Xây Dựng Bởi Trần Minh Thức";
+echo "Hệ Thống Được Xây Dựng Bởi Trần Minh Thức - Vui lòng Liên Hệ ADMIN để lấy quyền truy cập trang này!!!!!!!!!!!";
 ?>
+
+<<script>
+	alert("Bạn Không Có Quyền Truy Cập Trang Này!!!!!!!!!!!!!!!!!!")
+</script>
