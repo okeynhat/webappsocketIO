e96b8b3feb471028540db9881af68ddc4cbb88d4
okeynhat <okeynhat@users.noreply.github.com>
2020-01-30 15:22:15 +0700

    Update main.php

--- web/admin/main.php
+++ web/admin/main.php
@@ -1,12 +1,12 @@
 <?php
  
 // Require database & thông tin chung
-//require_once 'https://mdtiot.herokuapp.com/admin/core/init.php';
+require_once 'https://mdtiot.herokuapp.com/admin/core/init.php';
  
 // Require header
-//require_once '/includes/header.php';
+require_once '/includes/header.php';
  
 // Require footer
-//require_once 'https://mdtiot.herokuapp.com/admin/includes/footer.php';
+require_once 'https://mdtiot.herokuapp.com/admin/includes/footer.php';
  
 ?>
