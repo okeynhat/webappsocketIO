689136ed8a1243b6b788aed27a6812f9948b2e5a
okeynhat <okeynhat@users.noreply.github.com>
2020-01-30 15:16:30 +0700

    Update index.php

--- web/admin/index.php
+++ web/admin/index.php
@@ -4,7 +4,7 @@
 //require_once 'https://mdtiot.herokuapp.com/admin/core/init.php';
  
 // Require header
-require_once 'https://mdtiot.herokuapp.com/admin/includes/header.php';
+require_once '/includes/header.php';
  
 // Require footer
 //require_once 'https://mdtiot.herokuapp.com/admin/includes/footer.php';
