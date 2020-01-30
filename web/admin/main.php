de3992e8a051f7fa99fd63e0806df099a9128fc3
okeynhat <okeynhat@users.noreply.github.com>
2020-01-30 15:21:02 +0700

    Create main.php

---
+++ web/admin/main.php
@@ -0,0 +1,12 @@
+<?php
+ 
+// Require database & thông tin chung
+//require_once 'https://mdtiot.herokuapp.com/admin/core/init.php';
+ 
+// Require header
+//require_once '/includes/header.php';
+ 
+// Require footer
+//require_once 'https://mdtiot.herokuapp.com/admin/includes/footer.php';
+ 
+?>
