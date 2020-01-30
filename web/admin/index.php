1adfd21e2242b1914184250cba9da93966882fba
okeynhat <okeynhat@users.noreply.github.com>
2020-01-30 16:30:06 +0700

    Update index.php

--- web/admin/index.php
+++ web/admin/index.php
@@ -1,38 +1,18 @@
 <?php
-//Kiểm tra đăng nhập hay chưa
-require('db.php');
-session_start();
-session_register("capbac");
-if(isset($_SESSION["username"]) && $_SESSION["capbac"] == 0)
-{
-?>
-
+include("auth.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Trang Quản Lý</title>
+<title>Sharescript.net</title>
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <div class="form">
-<p>Xin chào <?php echo $_SESSION['capbac']; ?>!</p>
+<p>Xin chào <?php echo $_SESSION['username']; ?>!</p>
 <p>Đây là trang chủ</p>
 <p><a href="dashboard.php">Bảng điều khiển</a></p>
 <a href="logout.php">Đăng xuất</a>
 </div>
 </body>
 </html>
-<?php
-}
-else if(isset($_SESSION["username"]) && $_SESSION["capbac"] == 1)
-{
-	 echo "Không Phải Admin Không Có Quyền Vào Trang Này!!!!";
-}
-else if(!isset($_SESSION["username"]))
-{
-header("Location: login.php");
-exit();
-}
-	
-?>
