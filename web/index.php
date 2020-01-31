e2f3384138f332b1e29ec9eb4c215dc74312e419
okeynhat <okeynhat@users.noreply.github.com>
2020-01-31 10:56:15 +0700

    upd

--- web/index.php
+++ web/index.php
@@ -1,7 +1,18 @@
 <?php
-echo "Hệ Thống Được Xây Dựng Bởi Trần Minh Thức - Vui lòng Liên Hệ ADMIN để lấy quyền truy cập trang này!!!!!!!!!!!";
-?>
-
-<<script>
-	alert("Bạn Không Có Quyền Truy Cập Trang Này!!!!!!!!!!!!!!!!!!")
-</script>
+include("admin/auth.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Điều Khiển Nhà Thông Minh</title>
+<link rel="stylesheet" href="admin/css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Xin chào <?php echo $_SESSION['username']; ?>!</p>
+<p>Đây là trang chủ</p>
+<p><a href="dashboard.php">Bảng điều khiển</a></p>
+<a href="logout.php">Đăng xuất</a>
+</div>
+</body>
+</html>
