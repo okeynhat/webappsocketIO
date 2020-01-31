bff30d5d3f3e55a6263e649b571ccb24853ac0f5
okeynhat <okeynhat@users.noreply.github.com>
2020-01-31 15:45:08 +0700

    Update firebase.php

--- web/firebase.php
+++ web/firebase.php
@@ -25,17 +25,60 @@ include("admin/auth.php"); ?>
 
 <!-- Kẻ lưới giao diện -->
 
-<div class="container" style="background-color: grey; margin-bottom: 50px;">
-   <div class="row">
-      <div class="col-md-4 col-sm-6 col-xs-12">
-         <div class="khoi">dsgggggggggggg</div>
-      </div>
-      <div class="col-md-8 col-sm-6 col-xs-12">
-         <div class="khoi">dgsssssssssssss</div>
-      </div>
-   </div>
+<!-- list product -->
+<div class="container">
+	<div class="row mt-5">
+		<h2 class="list-product-title">New product</h2>
+		<div class="list-product-subtitle">
+			<p>List product description</p>
+		</div>
+		<div class="product-group">
+			<div class="row">
+				<div class="col-md-3 col-sm-6 col-12">
+					<div class="card card-product mb-3">
+					  <img class="card-img-top" src="http://via.placeholder.com/280x280" alt="Card image cap">
+					  <div class="card-body">
+					    <h5 class="card-title">Product</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-12">
+					<div class="card card-product mb-3">
+					  <img class="card-img-top" src="http://via.placeholder.com/280x280" alt="Card image cap">
+					  <div class="card-body">
+					    <h5 class="card-title">Product</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-12">
+					<div class="card card-product mb-3">
+					  <img class="card-img-top" src="http://via.placeholder.com/280x280" alt="Card image cap">
+					  <div class="card-body">
+					    <h5 class="card-title">Product</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-12">
+					<div class="card card-product mb-3">
+					  <img class="card-img-top" src="http://via.placeholder.com/280x280" alt="Card image cap">
+					  <div class="card-body">
+					    <h5 class="card-title">Product</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
 </div>
-
+<!-- end list product -->
 <!----------------------------------------------------------------->
 
 
