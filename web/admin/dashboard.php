978477b7e51e04b5052c09aecf5f0cbecaeb37b4
okeynhat <okeynhat@users.noreply.github.com>
2020-02-01 08:13:37 +0700

    y

--- web/admin/dashboard.php
+++ web/admin/dashboard.php
@@ -5,6 +5,7 @@ include("auth.php");?>
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width"> 
 <title>Bảng điều khiển </title>
 <link rel="stylesheet" href="css/style.css" />
 </head>
--- web/admin/login.php
+++ web/admin/login.php
@@ -4,6 +4,7 @@
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width"> 
 <title>Đăng nhập</title>
 <link rel="stylesheet" href="https://mdtiot.herokuapp.com/stylesheets/style.css" />
 </head>
