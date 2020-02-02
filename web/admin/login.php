d5365779dc5cefab5828d261b8a4d3183f035c00
okeynhat <okeynhat@users.noreply.github.com>
2020-02-02 17:25:29 +0700

    222

--- web/admin/login.php
+++ web/admin/login.php
@@ -6,7 +6,11 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width"> 
 <title>Đăng nhập</title>
-<link rel="stylesheet" href="https://mdtiot.herokuapp.com/stylesheets/style.css" />
+<link rel="stylesheet" href="https://mdtiot.herokuapp.com/admin/css/style.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet"> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
 <?php
@@ -29,14 +33,38 @@
 				}
     }else{
 ?>
-<div class="form">
-<h1>Đăng nhập</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Tên đăng nhập" required />
-<input type="password" name="password" placeholder="Mật khẩu" required />
-<input name="submit" type="submit" value="Đăng nhập" />
-</form>
-<p>Bạn chưa có tài khoản? <a id="dangky" onclick="dangky_click()">Đăng ký ngay</a></p><br/>
+<div class="container-fluid"> 
+ <div class="row-fluid"> 
+  <div class="col-md-offset-4 col-md-4" id="box"> 
+   <h2>Đăng Nhập</h2> 
+   <hr> 
+   <form class="form-horizontal" action="" method="post" id="login"> 
+    <fieldset> 
+     <div class="form-group"> 
+      <div class="col-md-12"> 
+       <div class="input-group"> <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span> <input name="username" placeholder="Tên Đăng Nhập" class="form-control" type="text"> 
+       </div> 
+      </div> 
+     </div> 
+     <div class="form-group"> 
+      <div class="col-md-12"> 
+       <div class="input-group"> <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span> <input name="password" placeholder="Mật Khẩu" class="form-control" type="password"> 
+       </div> 
+      </div> 
+     </div> 
+     <div class="form-group"> 
+      <div class="col-md-12"> 
+       <button type="submit" class="btn btn-md btn-danger pull-right">Đăng nhập </button> 
+       <p>Bạn chưa có tài khoản? <a id="dangky" onclick="dangky_click()">Đăng ký ngay</a></p><br/>
+      </div> 
+     </div> 
+    </fieldset> 
+   </form> 
+  </div> 
+ </div>
+</div>
+
+
 <?php } ?>
 </body>
 </html>
